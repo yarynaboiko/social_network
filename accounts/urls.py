bd12142e7437392d8f8d0b3b6c7454b29852f146
Yaryna Boiko <yaryna.boiko06@gmail.com>
2024-07-10 17:20:30 +0300

    Created new project

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from accounts import views
+
+urlpatterns = [
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('register/', views.UserRegisterView.as_view(), name='register'),
+]
