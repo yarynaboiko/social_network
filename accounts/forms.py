b118929b74bf8f5e3535329d5c7c08694bbe3657
Yaryna Boiko <yaryna.boiko06@gmail.com>
2024-08-13 16:28:54 +0300

    створила модель групи

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth.models import User
+from django import forms
+
+from accounts.models import User
 
 
 class UserLoginForm(AuthenticationForm):
@@ -16,10 +18,19 @@ class UserLoginForm(AuthenticationForm):
 class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'password1', 'password2')
+        fields = ('first_name', 'last_name', 'email', 'phone', 'gender', 'birthday', 'username', 'password1', 'password2')
+
+    help_texts = {
+        'username': None,
+        'password1': None,
+        'password2': None,
+    }
 
     def __init__(self, *args, **kwargs):
         super(UserRegisterForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({'class': 'form-control my-2'})
-        self.fields['password1'].widget.attrs.update({'class': 'form-control my-2'})
-        self.fields['password2'].widget.attrs.update({'class': 'form-control my-2'})
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control my-2'})
+            if self.errors.get(field_name):
+                field.widget.attrs.update({'class': 'form-control my-2 is-invalid'})
+
+        self.fields['birthday'].widget = forms.DateInput(attrs={"type": "date", 'class': "form-control mb-2"})
