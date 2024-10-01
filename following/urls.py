ea7d45624b4bd410f7e292bc1bb0ed21610eb14d
Yaryna Boiko <yaryna.boiko06@gmail.com>
2024-10-01 17:38:02 +0300

    створила модель групи

--- following/urls.py
+++ following/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('unsubscribe/<int:profile_id>', views.SubscriberDeleteView.as_view(), name='subscriber-delete'),
     path('friend_request/<int:profile_id>', views.FriendRequestCreateView.as_view(), name='friend-request-create'),
     path('friend_request/delete/<int:profile_id>', views.FriendRequestDeleteView.as_view(), name='friend-request-delete'),
+    path('friend_request/', views.FriendRequestListView.as_view(), name='friend-request-list'),
+    path('friend_request/<int:request_id>/accept', views.FriendCreateView.as_view(), name='friend-request-accept'),
+    path('friend_request/<int:request_id>/decline', views.FriendRequestDeclineView.as_view(), name='friend-request-decline'),
 
 ]
--- following/views.py
+++ following/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, get_object_or_404
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, DeleteView
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
+from django.views import View
+from django.views.generic import CreateView, DeleteView, ListView
 
 from accounts.models import User
-from following.models import Subscriber, FriendRequest
+from following.models import Subscriber, FriendRequest, Friend
 from profiles.models import Profile
 
 
@@ -47,6 +49,9 @@ class FriendRequestCreateView(LoginRequiredMixin, CreateView):
         form.instance.from_user = self.request.user
         profile = get_object_or_404(Profile, pk=self.kwargs['profile_id'])
         form.instance.to_user = profile.user
+        if form.instance.from_user == form.instance.to_user:
+            return redirect('profile-user-detail', pk=self.kwargs['profile_id'])
+        
         form.instance.save()
         return super().form_valid(form)
 
@@ -60,3 +65,28 @@ class FriendRequestDeleteView(LoginRequiredMixin, DeleteView):
 
     def get_success_url(self):
         return reverse_lazy('profile-user-detail', kwargs={'pk': self.kwargs['profile_id']})
+
+
+class FriendRequestListView(LoginRequiredMixin, ListView):
+    model = FriendRequest
+    template_name = 'following/friend-request-list.html'
+    context_object_name = 'friend_requests'
+
+    def get_queryset(self):
+        return FriendRequest.objects.filter(to_user = self.request.user, accepted=False)
+
+
+class FriendCreateView(LoginRequiredMixin, View):
+    def post(self, request, *args, **kwargs):
+        friend_request = get_object_or_404(FriendRequest, pk=self.kwargs['request_id'], to_user=self.request.user)
+        friend_request.accepted = True
+        friend_request.save()
+        friend = Friend.objects.create(from_user=friend_request.from_user, to_user=self.request.user)
+        return HttpResponseRedirect(reverse('friend-request-list'))
+
+
+class FriendRequestDeclineView(LoginRequiredMixin, View):
+    def post(self, request, *args, **kwargs):
+        friend_request = get_object_or_404(FriendRequest, pk=self.kwargs['request_id'], to_user=self.request.user)
+        friend_request.delete()
+        return HttpResponseRedirect(reverse('friend-request-list'))
